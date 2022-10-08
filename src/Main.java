07c9ebf67b08740c60e5b9da9b0f0207f723877c
Catalin <katalinus_katty@yahoo.com>
2022-10-08 23:46:15 +0300

    TemaLab9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println( " Hello world! ");
+
+        System.out.println( " Hello " );
+        System.out.println( " Catalin! ");
+
+        System.out.println(" Hello \n" +
+                           " Catalin ! ");
+
+        int adunare = 123 + 456;
+        System.out.println( adunare );
+
+        float impartire = 10 / 4;
+        System.out.println( impartire );
+
+        int rezultat1 = -5 + 8 * 6;
+        System.out.println( rezultat1 );
+
+        double rezultat2 = (55 + 9) % 9;
+        System.out.println( rezultat2 );
+
+        float rezultat3 = 20 + -3*5 /8;
+        System.out.println( rezultat3 );
+
+        float rezultat4 = 5 + 15/3*2 - 8 % 3;
+        System.out.println( rezultat4 );
+
+
+
+
+
+
+
+
+
+
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+    }
